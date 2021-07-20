25f16f9fdea825063484728300934c15a319ae61
omkargb <omkargb107@gmail.com>
2021-07-20 15:46:17 +0530

    [omkargb] Refractor : Calculation of daily employee wage

--- EmpWageCalc/ComputeWork.cs
+++ EmpWageCalc/ComputeWork.cs
@@ -6,16 +6,25 @@ namespace EmpWageCalc
 {
     public class ComputeWork
     {
-        public void EmpAttCheck(int empPresent)
+        /* public void EmpAttCheck(int empPresent)
         { 
             if (empPresent == 1)
             {
-                Console.WriteLine(" Employee is present. \n");
+                Console.WriteLine(" Employee is present.");
             }
             else
             {
-                Console.WriteLine(" Employee is absent. \n");
+                Console.WriteLine(" Employee is absent.");
             }
-        }    
-}
+        }
+        */
+
+        public void CalcDailywage()
+        {
+            int workHrs = 8;
+            int wagePerHr = 20;
+            int dailyWage = workHrs * wagePerHr;
+            Console.WriteLine(" Daily wage : "+dailyWage); 
+        }
+    }
 }
--- EmpWageCalc/Program.cs
+++ EmpWageCalc/Program.cs
@@ -7,14 +7,18 @@ namespace EmpWageCalc
         public static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Computation Program ");
+            ComputeWork cw = new ComputeWork();
 
-            //#UC1 : Checking employee attendance
+            /* #UC1 : Checking employee attendance
             Console.WriteLine(" [ Checking employee attendance ] "); 
             Random random = new Random();
-            int empPresent = random.Next(0, 2);
-
-            ComputeWork cw = new ComputeWork();   
+            int empPresent = random.Next(0, 2);     // generates 0 and 1
             cw.EmpAttCheck(empPresent);
+            */
+
+            //#UC2 : Calculate Daily Employee Wage
+            Console.WriteLine(" [ Calculating Daily Employee Wage ] ");
+            cw.CalcDailywage();
 
         }
     }
