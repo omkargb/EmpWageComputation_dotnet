336a03c4d42a9e2e74ab7cc31e52b6e9b5131510
omkargb <omkargb107@gmail.com>
2021-07-20 13:03:24 +0530

    [omkargb] Refractor : Employee attendance checking implemented

---
+++ EmpWageCalc/ComputeWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageCalc
+{
+    public class ComputeWork
+    {
+        public void EmpAttCheck(int empPresent)
+        { 
+            if (empPresent == 1)
+            {
+                Console.WriteLine(" Employee is present. \n");
+            }
+            else
+            {
+                Console.WriteLine(" Employee is absent. \n");
+            }
+        }    
+}
+}
--- EmpWageCalc/Program.cs
+++ EmpWageCalc/Program.cs
@@ -7,6 +7,15 @@ namespace EmpWageCalc
         public static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Computation Program ");
+
+            //#UC1 : Checking employee attendance
+            Console.WriteLine(" [ Checking employee attendance ] "); 
+            Random random = new Random();
+            int empPresent = random.Next(0, 2);
+
+            ComputeWork cw = new ComputeWork();   
+            cw.EmpAttCheck(empPresent);
+
         }
     }
 }
